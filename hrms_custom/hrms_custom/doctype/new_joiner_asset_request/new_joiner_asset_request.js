00d8e9736519e603063cfd1cad862dbc0234bf0c
Ashwin Kumar <ashwinkumark59@gmail.com>
2026-04-28 07:55:38 +0000

    fix: remove duplicate nested app causing doctype issue

--- hrms_custom/hrms_custom/doctype/new_joiner_asset_request/new_joiner_asset_request.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, ASHWIN and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("New Joiner Asset Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
