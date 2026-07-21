3684c74f2b7ea7c711d272d412729a3e5648da93
Ashwinkumar027 <ashwinkumark59@gmail.com>
2026-07-21 14:13:13 +0530

    Add signature/upload fields + mutual-exclusion to Form 11, fix print format; sync fixtures

--- hrms_custom/hrms_custom/doctype/form_11/form_11.py
+++ hrms_custom/hrms_custom/doctype/form_11/form_11.py
@@ -4,12 +4,12 @@ from frappe import _
 from frappe.model.document import Document
 from hrms_custom.hrms_custom.utils.signature_utils import merge_or_allow_insert
 
-
 class Form11(Document):
 	def validate(self):
 		self.validate_aadhar()
 		self.validate_pan()
 		self.validate_ifsc()
+		self.check_read_before_sign()
 
 	def validate_aadhar(self):
 		if self.aadhar_number and not re.fullmatch(r"\d{12}", self.aadhar_number):
@@ -22,6 +22,10 @@ class Form11(Document):
 	def validate_ifsc(self):
 		if self.ifsc_code and not re.fullmatch(r"[A-Z]{4}0[A-Z0-9]{6}", self.ifsc_code.upper()):
 			frappe.throw(_("Invalid IFSC Code format"))
+
+	def check_read_before_sign(self):
+		if self.signature and self.signature_upload:
+			frappe.throw(_("Please choose only one method to sign: either draw your signature or upload one, not both."))
 
 	def before_insert(self):
 		merge_or_allow_insert(self, route="Form11")
